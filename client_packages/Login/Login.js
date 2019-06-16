a168c79402230c2c3247fb628e2419b910281025
YorchCb <YorchCb@users.noreply.github.com>
2019-06-16 03:53:47 -0300

    f
    f

--- client_packages/Login/Login.js
+++
@@ -1,5 +0,0 @@
-$('#loginButton').click(() => {
-
-    $('.alert').remove(); //Remove any alerts when we attempt to login/register
-    mp.trigger('loginInformationToServer', $('#loginUsernameText').val(), $('#loginPasswordText').val());
-});
